2a14ec2a834a5f2934eb236067b13ced3a35d9df
Josh VanDeraa <jv@networktocode.com>
2022-06-10 21:36:23 -0500

    Trial

--- tasks.py
+++ tasks.py
@@ -7,12 +7,13 @@ from glob import glob
 @task(help={"template": "Name of the cookiecutter template to test"})
 def test(context, template=""):
     """Test a specific cookiecutter template."""
-    if not template:
-        sys.exit("-t / --template parameter is required!")
+    # if not template:
+    #     sys.exit("-t / --template parameter is required!")
     test_dir = pathlib.Path.cwd() / template / "tests"
     if test_dir.exists():
         print(f"Starting Test for {template}")
-        context.run(f"pytest { test_dir } -v --template { template }")
+        context.run(f"pytest { test_dir } -v")
+        # context.run(f"pytest { test_dir } -v --template { template }")
     else:
         sys.exit(f"No Test found for {template}")
 
